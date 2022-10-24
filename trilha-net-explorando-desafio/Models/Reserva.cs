ae4dd30bd47c4be71ecb0af02df39279217072dc
DevJsobral <109977062+DevJsobral@users.noreply.github.com>
2022-10-24 18:26:49 -0300

    Add files via upload

---
+++ trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,52 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new System.Exception("A capacidade da suite é menor que o número de hóspedes pretendido");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes(List<Pessoa> hospedes)
+        {
+            Hospedes = hospedes;
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = (DiasReservados * Suite.ValorDiaria);
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor = valor - (valor * 0.1M);
+            }
+
+            return valor;
+        }
+    }
+}
